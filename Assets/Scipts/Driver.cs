b2ed436512f8822916f7c71b9577bb055d5fc252
Ali <alilesani77549@gmail.com>
2023-08-16 14:08:45 +0330

    Adding Project Sprites

--- Assets/Scipts/Driver.cs
+++ Assets/Scipts/Driver.cs
@@ -9,7 +9,7 @@ public class Driver : MonoBehaviour
     [SerializeField] private float _steerSpeed = -0.25f;
     private void Update() {
         float moveAmount = Input.GetAxis("Vertical") * _moveSpeed;
-        float steerAmount = moveAmount != 0 ? Input.GetAxis("Horizontal") * _steerSpeed : 0;
+        float steerAmount = moveAmount != 0 ? Input.GetAxis("Horizontal") * -_steerSpeed : 0;
         _transform.Rotate(0, 0, Time.deltaTime * steerAmount);
         _transform.Translate(0, Time.deltaTime * moveAmount, 0);
     }
